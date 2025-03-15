b2e7cf783848890f7a1773d1b38b1a26c29bbf3a
Saminul Haque <saminulh@hotmail.com>
2025-03-15 16:07:24 -0700

    merge?

--- Mathlib/Probability/LindebergCLT.lean
+++ Mathlib/Probability/LindebergCLT.lean
@@ -3,8 +3,9 @@ Copyright (c) 2024 Thomas Zhu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Zhu, Rémy Degenne
 -/
-import Mathlib.MeasureTheory
+import Mathlib.MeasureTheory.Measure.Tight
 import Mathlib.MeasureTheory.Measure.ProbabilityMeasure
+import Mathlib.Probability.Distributions.Gaussian
 import Mathlib.MeasureTheory.Constructions.Pi
 import Mathlib.Probability.IdentDistrib
 
@@ -14,29 +15,10 @@ The Central Limit Theorem
 
 noncomputable section
 
-open MeasureTheory ProbabilityTheory Complex Filter
+open MeasureTheory ProbabilityTheory ProbabilityMeasure Complex Filter
 open scoped Real Topology
 
--- #check tendsto_one_plus_div_pow_exp
-/-- `(1 + t/n + o(1/n)) ^ n → exp t`. -/
-lemma tendsto_one_plus_div_pow_exp' {f : ℕ → ℂ} (t : ℝ)
-    (hf : (fun n ↦ f n - (1 + t / n)) =o[atTop] fun n ↦ 1 / (n : ℝ)) :
-    Tendsto (fun n ↦ f n ^ n) atTop (𝓝 (exp t)) := by
-  sorry
-
-lemma tendsto_sqrt_atTop : Tendsto (√·) atTop atTop := by
-  simp_rw [Real.sqrt_eq_rpow]
-  exact tendsto_rpow_atTop (by norm_num)
-
 variable {Ω : Type*} {mΩ : MeasurableSpace Ω} {X : ℕ → Ω → ℝ}
-
-/-- From PFR -/
-theorem iIndepFun_iff_pi_map_eq_map {Ω : Type u_1} {_mΩ : MeasurableSpace Ω}
-    {μ : Measure Ω} {ι : Type*} {β : ι → Type*} [Fintype ι]
-    (f : ∀ x : ι, Ω → β x) [m : ∀ x : ι, MeasurableSpace (β x)]
-    [IsProbabilityMeasure μ] (hf : ∀ (x : ι), Measurable (f x)) :
-    iIndepFun f μ ↔ Measure.pi (fun i ↦ μ.map (f i)) = μ.map (fun ω i ↦ f i ω) := by
-  sorry
 
 abbrev stdGaussian : ProbabilityMeasure ℝ :=
   ⟨gaussianReal 0 1, inferInstance⟩
@@ -59,11 +41,90 @@ lemma aemeasurable_invSqrtMulSum {μ : Measure Ω} (n) (hX : ∀ n, Measurable (
     AEMeasurable (invSqrtMulSum X n) μ :=
   (measurable_invSqrtMulSum n hX).aemeasurable
 
--- using ProbabilityMeasure for the topology
-variable {P : ProbabilityMeasure Ω}
 
-theorem central_limit (hX : ∀ n, Measurable (X n))
+lemma compact_supported_integral_tendsto_iff_tends_to
+  (P : ProbabilityMeasure Ω)
+  {Y : ℕ → Ω → ℝ}
+  (hY : ∀ n, Measurable (Y n)) (μ : ProbabilityMeasure ℝ):
+  (∀ {f : ℝ → ℝ}(hfcont: Continuous f) (hfcompact: HasCompactSupport f),
+    Tendsto (fun i ↦ ∫ (ω : ℝ), f ω ∂↑(P.map (hY i).aemeasurable)) atTop
+    (𝓝 (∫ (ω : ℝ), f ω ∂↑μ)))
+  ↔
+  Tendsto (fun n : ℕ => P.map (hY n).aemeasurable) atTop (𝓝 μ)
+  -- Goal: Tendsto (fun n ↦ P.map ⋯) atTop (𝓝 stdGaussian)
+
+  -- ∀ (f : BoundedContinuousFunction ℝ ℝ),
+  --   Tendsto (fun i ↦ ∫ (ω : ℝ), f ω ∂↑(P.map (hY i).aemeasurable)) atTop
+  --   (𝓝 (∫ (ω : ℝ), f ω ∂↑(P.map hZ.aemeasurable)))
+  := by
+  sorry
+  -- intro hctsconv
+  -- rw [tendsto_iff_forall_integral_tendsto]
+  -- intro f
+  -- -- have hfbdd := f.map_bounded'
+  -- obtain ⟨ C, hfbdd ⟩ := f.map_bounded'
+  -- let g (M : ℝ) (x : ℝ) := (f x) * (max 0 (min 1 (M+1-|x|)))
+
+  -- -- have h1 (f1 : ℝ → ℝ → ℝ) (hfcts: Continuous f1) : ∀ x : ℝ, Continuous (f1 x) := by
+
+  -- have : ∀ M : ℝ, M ≥ 0 → Continuous (g M) := by
+  --   intro M hM
+  --   -- continuity?
+  --   simp_all only [toMeasure_map, ge_iff_le, g]
+  --   apply Continuous.mul
+  --   · apply ContinuousMapClass.map_continuous
+  --   · apply Continuous.comp'
+  --     · apply Continuous.max
+  --       apply continuous_const
+  --       apply continuous_id
+  --     · apply Continuous.comp'
+  --       · apply Continuous.min
+  --         apply continuous_const
+  --         apply continuous_id
+  --       · apply Continuous.add
+  --         · apply continuous_const
+  --         · apply Continuous.comp'
+  --           · apply ContinuousNeg.continuous_neg
+  --           · apply Continuous.abs
+  --             apply continuous_id
+  -- sorry
+
+lemma smooth_compact_integral_tendsto_iff_compact_integral_tendsto
+  (P : ProbabilityMeasure Ω)
+  {Y : ℕ → Ω → ℝ}
+  (hY : ∀ n, Measurable (Y n)) (μ : ProbabilityMeasure ℝ):
+  (∀ {f : ℝ → ℝ}(hfsmooth: ContDiff ℝ ⊤ f) (hfcompact: HasCompactSupport f),
+    Tendsto (fun i ↦ ∫ (ω : ℝ), f ω ∂↑(P.map (hY i).aemeasurable)) atTop
+    (𝓝 (∫ (ω : ℝ), f ω ∂↑μ)))
+  ↔
+  (∀ {f : ℝ → ℝ}(hfcont: Continuous f) (hfcompact: HasCompactSupport f),
+    Tendsto (fun i ↦ ∫ (ω : ℝ), f ω ∂↑(P.map (hY i).aemeasurable)) atTop
+    (𝓝 (∫ (ω : ℝ), f ω ∂↑μ)))
+  -- ∀ (f : BoundedContinuousFunction ℝ ℝ),
+  --   Tendsto (fun i ↦ ∫ (ω : ℝ), f ω ∂↑(P.map (hY i).aemeasurable)) atTop
+  --   (𝓝 (∫ (ω : ℝ), f ω ∂↑(P.map hZ.aemeasurable)))
+  := sorry
+
+theorem smooth_compact_clt
+    (P : ProbabilityMeasure Ω)
+    (hX : ∀ n, Measurable (X n))
+    (h0 : P[X 0] = 0) (h1 : P[X 0 ^ 2] = 1)
+    (hindep : iIndepFun X P) (hident : ∀ (i : ℕ), IdentDistrib (X i) (X 0) P P)
+    {f : ℝ → ℝ}(hfsmooth: ContDiff ℝ ⊤ f) (hfcompact: HasCompactSupport f):
+    Tendsto (fun i ↦ ∫ (ω : ℝ), f ω ∂↑(P.map (aemeasurable_invSqrtMulSum i hX))) atTop
+    (𝓝 (∫ (ω : ℝ), f ω ∂↑stdGaussian)) := sorry
+
+theorem central_limit
+    (P : ProbabilityMeasure Ω)
+    (hX : ∀ n, Measurable (X n))
     (h0 : P[X 0] = 0) (h1 : P[X 0 ^ 2] = 1)
     (hindep : iIndepFun X P) (hident : ∀ (i : ℕ), IdentDistrib (X i) (X 0) P P) :
     Tendsto (fun n : ℕ => P.map (aemeasurable_invSqrtMulSum n hX)) atTop (𝓝 stdGaussian) := by
-  sorry
+  apply (compact_supported_integral_tendsto_iff_tends_to P
+    (fun n ↦ measurable_invSqrtMulSum n hX)
+    stdGaussian).mp
+  apply (smooth_compact_integral_tendsto_iff_compact_integral_tendsto P
+    (fun n ↦ measurable_invSqrtMulSum n hX)
+    stdGaussian).mp
+  intro f hfsmooth hfcompact
+  apply smooth_compact_clt P hX h0 h1 hindep hident hfsmooth hfcompact
